64e33225496977812df41179e709b3d8858c369c
Sang Le <vansang1706@gmail.com>
2017-07-26 10:54:00 +0700

    Fist commit to banch dev 1

---
+++ GitDemo1/GitDemo1/ClassDev1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo1
+{
+    class ClassDev1
+    {
+        public int Me { get; set; }
+    }
+}
